0b1c44e29fdeb141d257fe9351b63710e48b843e
Lei Zhu <uhziel@gmail.com>
2012-12-08 17:28:39 +0800

    gcd()添加断言，去除一些不可能发生的情况。测试一些新情况。

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
+#include <cassert>
 #include <gtest/gtest.h>
 
 unsigned int gcd(unsigned int x, unsigned int y)
 {
+  assert(x > 0);
+  assert(y > 0);
+
   unsigned int z = x % y;
   if (z == 0)
     return y;
@@ -12,4 +16,8 @@ unsigned int gcd(unsigned int x, unsigned int y)
 TEST(gcdtest, normal)
 {
   EXPECT_EQ(10, gcd(30, 20));
+  EXPECT_EQ(6, gcd(30, 12));
+  EXPECT_EQ(15, gcd(30, 15));
+  EXPECT_EQ(1, gcd(30, 1));
+  EXPECT_EQ(10, gcd(20, 30));
 }
